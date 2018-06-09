8e033a1d77afbb0f1f3f870b4ffd06ba6a59384b
hungdo2611 <tobmoki@gmail.com>
2018-06-09 11:50:42 +0700

    Revert "Hùng Đỗ"
    This reverts commit b9c286e09dc634ad7802cb48a227598c50d2292b.

--- QuanLyKhoHang/QuanLyKhoHang/KhoHang.cs
+++ QuanLyKhoHang/QuanLyKhoHang/KhoHang.cs
@@ -7,84 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
+
 namespace QuanLyKhoHang
 {
     public partial class KhoHang : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-SNTR6UK;Initial Catalog=QLKH;Integrated Security=True");
         public KhoHang()
         {
             InitializeComponent();
-        }
-        public void Show()
-        {
-            con.Open();
-            string sql = "SELECT *FROM KHOHANG";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            SqlDataReader dr = cmd.ExecuteReader();
-            DataTable dt = new DataTable();
-            dt.Load(dr);
-            dataGridView1.DataSource = dt;
-            con.Close();
-        }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            string sql = "INSERT INTO KHOHANG VALUES (@MAKHO, @TENKHO, @TONGSODMSP, @GHICHU)";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("MAKHO", txt_maKho.Text);
-            cmd.Parameters.AddWithValue("TENKHO", Txt_tenkho.Text);
-            cmd.Parameters.AddWithValue("TONGSODMSP", txt_soluong.Text);
-            cmd.Parameters.AddWithValue("GHICHU", txt_ghichu.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            Show();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            string sql = "UPDATE KHOHANG SET TENKHO = @TENKHO, TONGSODMSP = @TONGSODMSP, GHICHU = @GHICHU WHERE MAKHO = @MAKHO";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("MAKHO", txt_maKho.Text);
-            cmd.Parameters.AddWithValue("TENKHO", Txt_tenkho.Text);
-            cmd.Parameters.AddWithValue("TONGSODMSP", txt_soluong.Text);
-            cmd.Parameters.AddWithValue("GHICHU", txt_ghichu.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            Show();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            string sql = "DELETE FROM KHOHANG WHERE MAKHO = @MAKHO";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("MAKHO", txt_maKho.Text);
-            cmd.Parameters.AddWithValue("TENKHO", Txt_tenkho.Text);
-            cmd.Parameters.AddWithValue("TONGSODMSP", txt_soluong.Text);
-            cmd.Parameters.AddWithValue("GHICHU", txt_ghichu.Text);            
-            cmd.ExecuteNonQuery();
-            con.Close();
-            Show();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            string sql = "SELECT FROM KHOHANG WHERE MAKHO = @MAKHO";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("MAKHO", txt_timkiem.Text);
-            cmd.Parameters.AddWithValue("TENKHO", Txt_tenkho.Text);
-            cmd.Parameters.AddWithValue("TONGSODMSP", txt_soluong.Text);
-            cmd.Parameters.AddWithValue("GHICHU", txt_ghichu.Text);           
-            cmd.ExecuteNonQuery();
-            SqlDataReader dr = cmd.ExecuteReader();
-            DataTable dt = new DataTable();
-            dt.Load(dr);
-            dataGridView1.DataSource = dt;
-            con.Close();
         }
     }
 }
--- QuanLyKhoHang/QuanLyKhoHang/NhapXuat.cs
+++ QuanLyKhoHang/QuanLyKhoHang/NhapXuat.cs
@@ -7,99 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace QuanLyKhoHang
 {
-    public partial class PhieuNhap : Form
+    public partial class NhapXuat : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-SNTR6UK;Initial Catalog=QLKH;Integrated Security=True");
-        public PhieuNhap()
+        public NhapXuat()
         {
             InitializeComponent();
-        }
-        public void Show()
-        {
-            con.Open();
-            string sql = "SELECT *FROM PHIEUNHAPKHO";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            SqlDataReader dr = cmd.ExecuteReader();
-            DataTable dt = new DataTable();
-            dt.Load(dr);
-            dataGridView1.DataSource = dt;
-            con.Close();
-        }
-        
-        private void button2_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            string sql = "INSERT INTO PHIEUNHAPKHO VALUES (@MAPN, @MAKHO, @NVNHAP, @NGAYNHAP, @MANCC, @GHICHU)";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("MAPN",txt_maPN.Text);
-            cmd.Parameters.AddWithValue("MAKHO", txt_khohang.Text);
-            cmd.Parameters.AddWithValue("NVNHAP", txt_NV.Text);
-            cmd.Parameters.AddWithValue("NGAYNHAP", Txt_ngaynhap.Text);
-            cmd.Parameters.AddWithValue("MANCC", txt_nhacungcap.Text);
-            cmd.Parameters.AddWithValue("GHICHU", txt_ghichu.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            Show();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            string sql = "UPDATE PHIEUNHAPKHO SET MAKHO = @MAKHO, NVNHAP = @NVNHAP, NGAYNHAP = @NGAYNHAP, MANCC = @MANCC, GHICHU = @GHICHU WHERE MAPN = @MAPN";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("MAPN", txt_maPN.Text);
-            cmd.Parameters.AddWithValue("MAKHO", txt_khohang.Text);
-            cmd.Parameters.AddWithValue("NVNHAP", txt_NV.Text);
-            cmd.Parameters.AddWithValue("NGAYNHAP", Txt_ngaynhap.Text);
-            cmd.Parameters.AddWithValue("MANCC", txt_nhacungcap.Text);
-            cmd.Parameters.AddWithValue("GHICHU", txt_ghichu.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            Show();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            string sql = "DELETE FROM PHIEUNHAPKHO WHERE MAPN = @MAPN";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("MAPN", txt_maPN.Text);
-            cmd.Parameters.AddWithValue("MAKHO", txt_khohang.Text);
-            cmd.Parameters.AddWithValue("NVNHAP", txt_NV.Text);
-            cmd.Parameters.AddWithValue("NGAYNHAP", Txt_ngaynhap.Text);
-            cmd.Parameters.AddWithValue("MANCC", txt_nhacungcap.Text);
-            cmd.Parameters.AddWithValue("GHICHU", txt_ghichu.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            Show();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            string sql = "SELECT FROM PHIEUNHAPKHO WHERE MAPN = @MAPN";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("MAPN", txtTimKIem.Text);
-            cmd.Parameters.AddWithValue("MAKHO", txt_khohang.Text);
-            cmd.Parameters.AddWithValue("NVNHAP", txt_NV.Text);
-            cmd.Parameters.AddWithValue("NGAYNHAP", Txt_ngaynhap.Text);
-            cmd.Parameters.AddWithValue("MANCC", txt_nhacungcap.Text);
-            cmd.Parameters.AddWithValue("GHICHU", txt_ghichu.Text);
-            cmd.ExecuteNonQuery();
-            SqlDataReader dr = cmd.ExecuteReader();
-            DataTable dt = new DataTable();
-            dt.Load(dr);
-            dataGridView1.DataSource = dt;
-            con.Close();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
     }
 }
