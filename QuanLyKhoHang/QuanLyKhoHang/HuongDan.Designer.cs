6e937ad95e4c1ad12114eac98a4a1cc6e0ca2b2a
maithilinh <36099194+maithilinh@users.noreply.github.com>
2018-06-11 11:55:36 +0700

    HDSDupdate

--- QuanLyKhoHang/QuanLyKhoHang/HuongDan.Designer.cs
+++ QuanLyKhoHang/QuanLyKhoHang/HuongDan.Designer.cs
@@ -28,20 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Nhà cung cấp");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Khách hàng");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Sản phẩm");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Phiếu nhập");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Phiếu xuất");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Nhân viên");
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 16);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(650, 513);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(3, 16);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Nhà cung cấp";
+            treeNode2.Name = "Node0";
+            treeNode2.Text = "Khách hàng";
+            treeNode3.Name = "Node1";
+            treeNode3.Text = "Sản phẩm";
+            treeNode4.Name = "Node2";
+            treeNode4.Text = "Phiếu nhập";
+            treeNode5.Name = "Node3";
+            treeNode5.Text = "Phiếu xuất";
+            treeNode6.Name = "NhanVien";
+            treeNode6.Text = "Nhân viên";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            this.treeView1.Size = new System.Drawing.Size(198, 516);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.webBrowser1);
+            this.groupBox2.Location = new System.Drawing.Point(212, 15);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(656, 532);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chi tiết";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 535);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hướng dẫn";
             // 
             // HuongDan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(873, 565);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "HuongDan";
             this.Text = "HuongDan";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HuongDan_FormClosed);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- QuanLyKhoHang/QuanLyKhoHang/HuongDan.cs
+++ QuanLyKhoHang/QuanLyKhoHang/HuongDan.cs
@@ -23,5 +23,21 @@ namespace QuanLyKhoHang
             MainMenu mn = new MainMenu();
             mn.ShowDialog();
         }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text == "Nhà cung cấp")
+                webBrowser1.Navigate(@"D:\Kỳ 6\Thực tập nhóm_TH14\QLKhoHang\QuanLyKhoHang\QuanLyKhoHang\Guide\Nhacungcap.html");
+            if (e.Node.Text == "Khách hàng")
+                webBrowser1.Navigate(@"D:\Kỳ 6\Thực tập nhóm_TH14\QLKhoHang\QuanLyKhoHang\QuanLyKhoHang\Guide\Khachhang.html");
+            if (e.Node.Text == "Sản phẩm")
+                webBrowser1.Navigate(@"D:\Kỳ 6\Thực tập nhóm_TH14\QLKhoHang\QuanLyKhoHang\QuanLyKhoHang\Guide\Sanpham.html");
+            if (e.Node.Text == "Phiếu xuất")
+                webBrowser1.Navigate(@"D:\Kỳ 6\Thực tập nhóm_TH14\QLKhoHang\QuanLyKhoHang\QuanLyKhoHang\Guide\Phieuxuat.html");
+            if (e.Node.Text == "Phiếu nhập")
+                webBrowser1.Navigate(@"D:\Kỳ 6\Thực tập nhóm_TH14\QLKhoHang\QuanLyKhoHang\QuanLyKhoHang\Guide\Phieunhap.html");
+            if (e.Node.Text == "Nhân viên")
+                webBrowser1.Navigate(@"D:\Kỳ 6\Thực tập nhóm_TH14\QLKhoHang\QuanLyKhoHang\QuanLyKhoHang\Guide\NhanVien.html");
+        }
     }
 }
